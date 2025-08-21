cbc3a1105048ee53a7206e65fdb9d905b4049fc9
Imansoft <Imansofttechnologies@gmail.com>
2025-08-21 21:48:53 +0100

    Progressive Web App

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'exectrack-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './roadmap.html',
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png',
+  // Add your CSS, JS, and icon files here
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
